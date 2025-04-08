affac604a43b8158517eb74f1c467021da05a944
SaikatDeb12 <saikat.rcld@gmail.com>
2025-04-08 09:38:19 +0530

    defining schema

---
+++ model/user.js
@@ -0,0 +1,25 @@
+const { Schema } = require("mongoose");
+const useSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
